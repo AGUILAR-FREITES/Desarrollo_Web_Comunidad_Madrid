f008d7f09a423377dd67dbde4279eff1147f4904
AGUILAR-FREITES <aguilarfreites@gmail.com>
2026-05-21 00:38:43 +0200

    Desarroll el Form con JS

---
+++ bootstrap/js/login.js
@@ -0,0 +1,15 @@
+
+
+function login(){
+    let usuario=document.Login.usuario.value;
+    let password=document.Login.password.value;
+    
+
+    if(usuario=="luis" && password=="123"){
+    
+        window.open("/Desarrollo_Web_Comunidad_Madrid/bootstrap/html/Inventario.html", "_blank");
+
+    }
+}
+
+
